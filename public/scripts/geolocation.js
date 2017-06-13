d8f4b8ddf1e191cc392c9566b460cd6a829ee4aa
Henrik Fogelberg <henrik.fogelberg@gmail.com>
2017-06-13 11:30:27 +0100

    Added geolocation

---
+++ public/scripts/geolocation.js
@@ -0,0 +1,30 @@
+
+function getLocation() {
+  if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        console.log(position);
+        var lat = position.coords.latitude;
+        var lon = position.coords.longitude;
+        console.log("lat: " + lat);
+        console.log("Lon: " + lon);
+        handleGeoDat(lat, lon);
+
+      });
+    } else { 
+      handleNoGeolocation(true)
+  }
+}
+
+function handleGeoDat(lat, lon) {
+  var url = "/forecast/"+lat+"/"+lon;
+  window.location.replace(url);
+}
+
+function handleNoGeolocation(errFlag) {
+  if (errorFlag) {
+    var content = 'Error: The Geolocation service failed.';
+  } else {
+    var content = 'Error: Your browser doesn\'t support geolocation.';
+  }
+  alert(content);
+}
